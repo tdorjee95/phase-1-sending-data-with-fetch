0876da436836d7c2975e9c8aa42a404ece28ef84
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-11-28 23:57:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+    const userData = {
+      name: userName,
+      email: userEmail,
+    };
+  
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then((response) => response.json())
+      .then((user) => {
+  
+        const idNum = document.createElement("p");
+        idNum.innerHTML = user.id;
+  
+        document.body.appendChild(idNum);
+      })
+      .catch((error) => {
+        const errorMessage = document.createElement("p");
+        errorMessage.innerHTML = error.message;
+  
+        document.body.appendChild(errorMessage);
+      });
+  }
+  
